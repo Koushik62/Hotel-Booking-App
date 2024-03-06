fbdcbe135301eefeab59a28c09af2fb63068f3ac
Koushik <gurramkoushik8@gmail.com>
2024-03-07 00:38:34 +0530

    auth , login & registration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose'
+import userRoutes from "./routes/users"
 
 const app = express();
 app.use(express.json());
@@ -10,10 +11,7 @@ app.use(cors());
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
-
-app.get("/api/test", async(req,res)=>{
-    res.json({message:"hello from express endpoint!"});
-});
+app.use("/api/users", userRoutes);
 
 app.listen(7000,()=>{
     console.log("server running on local host 7000");
